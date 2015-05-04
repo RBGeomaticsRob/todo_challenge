5fd3c78f765a88a243ea48069dd3aa95cb138d4e
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-04 14:36:05 +0100

    Added test for adding an item to the list

--- test/e2e/captureFeature.js
+++ test/e2e/captureFeature.js
@@ -4,4 +4,10 @@ describe('capture', function(){
     expect(browser.getTitle()).toEqual('Capture');
   });
 
+  it('can add a todo item to the list', function(){
+    element(by.model('taskCtrl.loadTask')).sendKeys('A test Task.');
+    element(by.className('form__button--submit')).click();
+    expect(element.all(by.repeater('task in taskCtrl.tasks').get(0).getText())).toEqual('A test Task.');
+  });
+
 });
