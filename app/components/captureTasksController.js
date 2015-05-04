688ddbe2e2c7d34149d4d5402bf7ddd53dbc8e00
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-04 15:27:07 +0100

    unit tests passed to add taks to list of tasks

--- app/components/captureTasksController.js
+++ app/components/captureTasksController.js
@@ -1,3 +1,10 @@
 app.controller('CaptureTasksController', [function(){
-
+  var self = this
+  this.tasks = []
+  this.addTask = function(task){
+    self.tasks.push({
+      'desc': task,
+      'createdAt': Date.now()
+    })
+  }
 }]);
--- test/unit/captureTasksController.spec.js
+++ test/unit/captureTasksController.spec.js
@@ -10,4 +10,14 @@ describe('CaptureTasksController', function(){
   it('initialises with an empty task load', function(){
     expect(ctrl.loadTask).toBeUndefined();
   });
+
+  it('can add a task to the list', function(){
+    ctrl.addTask('A new Task');
+    expect(ctrl.tasks[0].desc).toEqual('A new Task')
+  });
+
+  it('can date stamp a task', function(){
+    ctrl.addTask('A new Task');
+    expect(ctrl.tasks[0].createdAt).toEqual(Date.now())
+  });
 });
